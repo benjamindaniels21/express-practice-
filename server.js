a461a5c081e7b83d858a7a23e71ace5505709c14
Ben Daniels <benjamindaniels21@gmail.com>
2022-05-03 20:51:33 -0400

    nodemon added andnd various routes added

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ app.get("/", function (req, res) {
   res.send("<h1>Hello World</h1>");
 });
 
+app.get("/contact", function (req, res) {
+  res.send("<h1>Contact Page</h1>");
+});
+
+app.get("/about", function (req, res) {
+  res.send("<h1>Ben Daniels</h1> <p>A human man learning web development</p>");
+});
+
+app.get("/hobbies", function (req, res) {
+  res.send("<h1>Hobbies</h1> <p>Making Things</p>");
+});
+
 app.listen(3000, function () {
   console.log("sever started on port 3000");
 });
